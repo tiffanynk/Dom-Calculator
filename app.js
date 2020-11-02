acb15c734c734549863323f112b1c5aeddc88aa7
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-11-02 12:18:50 -0700

    solution WITH domcontent loaded and refactoring

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
-// THIS CODE WORKS WITH SCRIPT DEFER SRC IN HTML
-const screen = document.querySelector('#screen')
-const buttons = document.querySelectorAll('span')
-const buttonsDiv = document.querySelector('#buttons-container')
+document.addEventListener('DOMContentLoaded', () => {
+    const screen = document.querySelector('#screen');
+    const buttons = document.querySelectorAll('span');
+    const buttonsDiv = document.querySelector('#buttons-container');
+    const clear = document.querySelector('#clear');
 
-const clear = document.getElementById('clear')
+    //dynamic dispatch vs switch
+    const operations = {
+        '=': calculateResponse,
+        'C': clearValue,
+        default: inputExpression => screen.textContent += inputExpression,
+    }
 
-let calculatorString = "";
+    // operations["="] || operations.C get back a function
 
-buttonsDiv.addEventListener("click", (event) => {
-    let calculatorString = event.target.textContent
-    screen.append(calculatorString)
+    buttonsDiv.addEventListener('click', (event) => {
+        let inputExpression = event.target.textContent
 
-    if (calculatorString === "C"){
-        clearValue()
-    } 
-    if (calculatorString === '='){
-        calculateResponse()
+
+        operation = operations[inputExpression] || operations['default']
+        operation(inputExpression)
+
+        //if/else vs switch
+        // if (inputExpression === "C"){
+        //     clearValue()
+        // } else if (inputExpression === '='){
+        //     calculateResponse()
+        // } else { //this line allows us to delete the slice from line 28
+        //     screen.append(inputExpression)
+        // }
+    })
+
+    function clearValue(){
+        screen.innerHTML = ''
+    }
+
+    function calculateResponse(){
+        const forumlaString = screen.textContent
+        const correctString = forumlaString
+            .replace(/x/g, '*')
+            .replace(/÷/g, '/')
+        const result = eval(correctString)
+        screen.textContent = result == 'Infinity'
+            ? 'NOPE'
+            : result
     }
 })
-
-function clearValue(){
-    screen.innerHTML = ''
-}
-
-function calculateResponse(){
-    const forumlaString = screen.textContent.slice(0, -1)
-    const correctString = forumlaString.replace(/x/g, '*').replace(/÷/g, '/')
-    const result = eval(correctString)
-    clearValue()
-    screen.append(result)
-}
